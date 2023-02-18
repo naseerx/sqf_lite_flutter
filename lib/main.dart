471c78c2e2bbd6239ce0b0e55bfb62b0280513ec
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2023-02-18 22:15:55 +0500

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My SqfLite',
+      title: 'My SqfLite App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
